83b2cabb5c35329bf14698c84cdf113a60a55c75
durjoy sarkar <durjoywebdeveloper777@gmail.com>
2022-04-03 05:38:00 +0600

    hello react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
+import "./App.css";
+import Home from './Components/Home/Home';
+
+function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<Home></Home>}></Route>
+    </Routes>
+  );
+}
+
+export default App;
