98bf9fb290911dae42f3d433db32ea1deec27419
durjoy sarkar <durjoywebdeveloper777@gmail.com>
2022-04-03 05:50:36 +0600

    navbar add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import { Route, Routes } from 'react-router-dom';
 import "./App.css";
 import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
+import Navbar from './Components/Navbar/Navbar';
+import NotFound from './Components/NotFound/NotFound';
+import Videos from './Components/Videos/Videos';
 
 function App() {
   return (
+    <>
+    <Navbar></Navbar>
     <Routes>
       <Route path='/' element={<Home></Home>}></Route>
+      <Route path='/videos' element={<Videos></Videos>}></Route>
+      <Route path='/Login' element={<Login></Login>}></Route>
+      <Route path='*' element={<NotFound></NotFound>}></Route>
     </Routes>
+    </>
   );
 }
 
